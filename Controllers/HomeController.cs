d67e366573550c7ca4cdb6c35693321cc9188d15
TOMÁS LEONEL DEGESE <47349708@ort>
2023-08-10 14:14:34 -0300

    Se viene reventon

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,10 +18,37 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
+public IActionResult ConfigurarJuego()
+{
+    ViewBag.Categorias = BD.ObtenerCategorias();
+    ViewBag.Dificultades = BD.ObtenerDificultades();
+    Juego.InicializarJuego();
+    return  View();
+}
+
+public IActionResult Comenzar(string username,int dificultad,int categoria)
+{
+ Juego.CargarPartida(username,dificultad,categoria);   
+ if(_preguntas.Count > 0){
+    return view(CargarPartida)
+ }else
+ {return view(ConfigurarJuego)}
+}
+
+public IActionResult Jugar(){
+
+}
+
+[HttpPost] public IActionResult VerificarRespuesta(int idPregunta, int idRespuesta){
+    bool Vericado = VerificarRespuesta(idPregunta,idRespuesta);
+    ViewBag.Vericado = Vericado;
+    if(!Vericado){
+//int correcto = ObtenerRespuestaCorrecta(idPregunta)}
+//ViewBag.correcto = Correcto
     }
+}
+
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
