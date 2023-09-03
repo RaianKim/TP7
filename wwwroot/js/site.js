f405a5276b022c08db0d73a85d462f74ccb01f4f
RaianKim <raiank2007@gmail.com>
2023-09-03 02:03:17 -0300

    .

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,27 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const radioButtons = document.querySelectorAll('.radio-label-cj .cjradio');
+radioButtons.forEach(function(radioButton) {
+    radioButton.addEventListener('click', function() {
+        const allLabels = document.querySelectorAll('.radio-label-cj ');
+        allLabels.forEach(function(label) {
+            label.classList.remove('btn-danger');
+            label.classList.add('btn-default');
+        });
+        const parentLabel = this.closest('.radio-label-cj ');
+        parentLabel.classList.remove('btn-default');
+        parentLabel.classList.add('btn-danger');
+    });
+});
 
-// Write your JavaScript code.
+const radioButtons2 = document.querySelectorAll('.radio-label-cj2 .cjradio2');
+radioButtons2.forEach(function(radioButton) {
+    radioButton.addEventListener('click', function() {
+        const allLabels = document.querySelectorAll('.radio-label-cj2 ');
+        allLabels.forEach(function(label) {
+            label.classList.remove('btn-danger');
+            label.classList.add('btn-default');
+        });
+        const parentLabel = this.closest('.radio-label-cj2 ');
+        parentLabel.classList.remove('btn-default');
+        parentLabel.classList.add('btn-danger');
+    });
+});
